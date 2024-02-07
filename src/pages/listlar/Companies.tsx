f70fd818f87e8e04784694d77c43d3968c1d7cb5
Rasuljonov02 <muhammadjonrasuljonov2002@gmail.com>
2024-02-07 11:04:50 +0500

    fbsdgugs

--- src/pages/listlar/Companies.tsx
+++ src/pages/listlar/Companies.tsx
@@ -1,15 +1,33 @@
 import { Button, Table } from 'antd';
 import { useFetch } from 'pages/useFetch/useFetch';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 
 import plus from '../../img/plus.svg';
+import Info from 'pages/madallar/info';
 interface UsersProps {}
 
 const Companies: FunctionComponent<UsersProps> = () => {
   const { data = [], isLoading, isFetching, refetch } = useFetch({ url: '/shops' });
+  const [infi, setInfi] = useState<any>(null);
+  const [infobol, infobolean] = useState<boolean>(false);
+
+  const handleEdit = (record: any) => {
+    setInfi(record);
+    infobolean(true);
+    console.log('Edit', record);
+  };
+
+  const handleDelete = (record: any) => {
+    console.log('Delete', record);
+  };
+
+  if(infobol){
+    return <Info data={infi}/>
+  }
 
   return (
     <div className="container w-full  py-2">
+      
       <div className="flex items-center justify-between rounded-[10px] bg-slate-200 p-2 text-white">
         <div>
           <img className="w-[25px] cursor-pointer" src={plus} alt="" />
@@ -31,7 +49,20 @@ const Companies: FunctionComponent<UsersProps> = () => {
           { title: 'Title', dataIndex: 'title' },
           { title: 'Location', dataIndex: 'location' },
           { title: 'Phone Number', dataIndex: 'phone' },
-          { title: 'Shop Number', dataIndex: 'number' }
+          { title: 'Shop Number', dataIndex: 'number' },
+          {
+            title: 'Actions',
+            render: (i, record) => (
+              <div className='flex items-center gap-2'>
+                <Button type="primary" onClick={() => handleEdit(record)}>
+                  Edit
+                </Button>
+                <Button type="primary" onClick={() => handleDelete(record)}>
+                  Delete
+                </Button>
+              </div>
+            )
+          }
         ]}
       />
     </div>
---
+++ src/pages/madallar/info.tsx
@@ -0,0 +1,81 @@
+import { Button, Input } from 'antd';
+import { IApi } from '../../modules/auth/types';
+import Companies from 'pages/listlar/Companies';
+import { FC, useState } from 'react';
+import { session } from 'services';
+
+interface InfoProps {
+  data: any;
+}
+
+const Info: FC<InfoProps> = ({ data }) => {
+  const [infobol, setInfobol] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>(data.title || '');
+  const [location, setLocation] = useState<string>(data.location || '');
+  const [phone, setPhone] = useState<string>(data.phone || '');
+  const [number, setNumber] = useState<string>(data.number || '');
+  const [id, idset] = useState<string>(data._id || '');
+
+  const save = () => {
+    let myHeaders = new Headers();
+    myHeaders.append('x-auth-token', session.get());
+
+    
+
+    const payload = {
+      title: title,
+      location: location,
+      phone: phone,
+      number: number
+    };
+
+    let requestOptions: any = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: JSON.stringify(payload),
+      redirect: 'follow'
+    };
+
+    console.log('salom', requestOptions);
+
+    fetch(`http://137.184.188.134:4000/api/shops/${id}`, requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  };
+
+  const handleEdit = () => {
+    setInfobol(true);
+  };
+
+  if (infobol) {
+    return <Companies />;
+  }
+ 
+  return (
+    <div className="mt-2 w-full">
+      <Button type="primary" onClick={handleEdit}>
+        Next
+      </Button>
+      <div className="mt-4 flex w-full gap-2">
+        <div className="flex w-[200px] flex-col gap-2">
+          <p>Joy name</p>
+          <Input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+          <p>location</p>
+          <Input type="text" value={location} onChange={e => setLocation(e.target.value)} />
+        </div>
+        <div className="flex w-[200px] flex-col gap-2">
+          <p>phone</p>
+          <Input type="number" value={phone} onChange={e => setPhone(e.target.value)} />
+          <p>docon number</p>
+          <Input type="number" value={number} onChange={e => setNumber(e.target.value)} />
+        </div>
+      </div>
+      <Button type="primary" onClick={save}>
+        Save
+      </Button>
+    </div>
+  );
+};
+
+export default Info;
